cd60f293b18fcb8d9b8c9c3adc577a43cbb9b5b3
Jasmir Medina <201848984+fn-cafeina@users.noreply.github.com>
2026-02-03 12:09:06 -0600

    Finish main logic

---
+++ src/js/main.js
@@ -0,0 +1,34 @@
+const Gameboard = (function () {
+  const board = Array(9).fill(0);
+
+  const draw = function (mark, position) {
+    if (!board[position]) {
+      board[position] = mark;
+    }
+  };
+
+  const combs = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [6, 4, 2],
+  ];
+
+  const win = function () {
+    for (const comb of combs) {
+      if (
+        board[comb[0]] === board[comb[1]] &&
+        board[comb[1]] === board[comb[2]] &&
+        board[comb[0]] !== 0
+      ) {
+        return true;
+      }
+    }
+  };
+
+  return { draw, win };
+})();
